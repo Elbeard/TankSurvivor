6168b8d9512096df4b55bab0a1940f4c4c69ab68
Elektrobeard <elektrobeard@gmail.com>
2024-08-23 22:40:04 +0700

    tower rotation to cursor

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,69 +2,82 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
-    private Transform playerTransform;
+    private PlayerInputActions _playerInputActions;
+    private Rigidbody2D _rigidBody;
+    [SerializeField]     
+    private Transform _towerTransform;
     [SerializeField]
-    private float movingSpeed = 1f;
+    private float _movingSpeed = 1f;
     [SerializeField]
-    private float rotationSpeed = 1f;
+    private float _rotationSpeed = 1f;
     [SerializeField]
-    private float smoothTime = 0.3F;
-    private Vector2 smoothedMovementInput = Vector2.zero;
-    private Vector2 movementInputSmoothVelocity = Vector2.zero;
-    private Vector2 movementInput = Vector2.zero;
+    private float _smoothTime = 0.3F;
+    private Vector2 _smoothedMovementInput = Vector2.zero;
+    private Vector2 _movementInputSmoothVelocity = Vector2.zero;
+    private Vector2 _movementInput = Vector2.zero;
+    private Vector2 _mousePosition;
+    private Vector3 _worldMousePosition;
 
     private void Awake()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
-        playerTransform = GetComponent<Transform>();
+        _playerInputActions = new PlayerInputActions();
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _playerInputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _playerInputActions.Disable();
     }
 
     private void FixedUpdate()
     {
-        //HandleMovement();
-        //TowerRotation();
+        TowerRotation();
         SetPlayerVelocity();
         RotateInDirectionOfInput();
     }
 
     public void OnMove(InputValue value)
     {
-        movementInput = value.Get<Vector2>();
+        _movementInput = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        _mousePosition = value.Get<Vector2>();
     }
 
     private void RotateInDirectionOfInput()
     {
-        if (movementInput != Vector2.zero)
+        if (_movementInput != Vector2.zero)
         {
-            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, smoothedMovementInput);
-            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
-            rigidBody.MoveRotation(rotation);
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime);
+            _rigidBody.MoveRotation(rotation);
         }
     }
 
     private void SetPlayerVelocity()
     {
-        smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, movementInput, ref movementInputSmoothVelocity, smoothTime * Time.fixedDeltaTime);
-        rigidBody.velocity = smoothedMovementInput * movingSpeed;
-
-    }
-
-    private void HandleMovement()
-    {
-        Vector2 movenentVector = GameInput.Instance.GetMovmentVector();
-        movenentVector = movenentVector.normalized;
-        rigidBody.MovePosition(rigidBody.position + movenentVector * (movingSpeed * Time.fixedDeltaTime));
+        _smoothedMovementInput = Vector2.SmoothDamp(_smoothedMovementInput, _movementInput, ref _movementInputSmoothVelocity, _smoothTime * Time.fixedDeltaTime);
+        _rigidBody.velocity = _smoothedMovementInput * _movingSpeed;
     }
 
     private void TowerRotation()
     {
-        Vector3 rotateVector = GameInput.Instance.GetRotateVector();
-
-        playerTransform.Rotate(Vector3.forward * rotateVector.x);
-        Debug.Log(rotateVector);
+        _worldMousePosition = Camera.main.ScreenToWorldPoint(new Vector3(_mousePosition.x, _mousePosition.y, Camera.main.nearClipPlane));
+        Vector3 direction = _worldMousePosition - transform.position;
+        direction.z = 0; 
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        _towerTransform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        Debug.Log($"_mousePosition ={_mousePosition}, _worldMousePosition = {_worldMousePosition}, direction = {direction}, angle = {angle}");
     }
 }
